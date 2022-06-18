77f4993a1b7d2df940b224167c07e41d9097cacd
Vaishnavi-janagave <96948581+Vaishnavi-janagave@users.noreply.github.com>
2022-06-18 14:53:33 +0530

    Add files via upload

---
+++ Final project Restaurant website/resturantjs.js
@@ -0,0 +1,10 @@
+// let dabba =  document.getElementById('ka').onclick = function()
+// {myFunction()};
+
+// function myFunction(){
+//     document.getElementById('pan').innerHTML = 'jiji';
+// }
+
+
+// let submit = document.getElementById('submit').innerHTML() 
+ 
